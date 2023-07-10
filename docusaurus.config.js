2511642cfbd0f8e8be448e05be9dd0badc0e0210
Raul <rauldiazlarraga@gmail.com>
2023-07-10 12:30:11 +0200

    change test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Raul Diaz site',
-  tagline: 'Raul Diaz Site',
+  tagline: 'Raul Diaz Site hola test',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
